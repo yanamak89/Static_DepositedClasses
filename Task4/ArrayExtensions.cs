b97fcca0ca4dc7abbe6ae331593922d037a1cb04
Yana Makogon <yana.mak89@gmail.com>
2024-06-03 12:38:02 +0200

    tasks completed

--- Task4/ArrayExtensions.cs
+++ Task4/ArrayExtensions.cs
@@ -1,6 +1,26 @@
 namespace Task4;
 
-public class ArrayExtensions
+public static class ArrayExtensions
 {
-    
+    public static void SortAscending(this int[] array)
+    {
+        Array.Sort(array);
+    }
+
+    public static void Show()
+    {
+        int[] array = { 5, 3, 9, 2, 1, 5, 8, 10 };
+        Console.WriteLine("Original array:");
+        foreach (var num in array)
+        {
+            Console.Write(num + " ");
+        }
+
+        array.SortAscending();
+        Console.WriteLine("\nSorted array:");
+        foreach (var num in array)
+        {
+            Console.Write(num + " ");
+        }
+    }
 }
--- Task6/Calculator.cs
+++ Task6/Calculator.cs
@@ -1,6 +1,76 @@
 namespace Task6;
 
-public class Calculator
+public static class Calculator
 {
+    public static double Add(double num1, double num2)
+    {
+        double result = num1 + num2;
+        return result;
+    }
+
+    public static double Subtraction(double num1, double num2)
+    {
+        double result = num1 - num2;
+        return result;
+    }
     
+    public static double Multiplication(double num1, double num2)
+    {
+        double result = num1 * num2;
+        return result;
+    }
+    
+    public static double Division(double num1, double num2)
+    {
+        if (num2 == 0)
+        {
+            Console.WriteLine("Error: You cant divide by zero!");
+            return double.NaN; // повернути значення, яке вказує на помилку
+        }
+        double result = num1 / num2;
+        return result;
+        
+    }
+    
+    public static double ReminderOfTheDivision(double num1, double num2)
+    {
+        if (num2 == 0)
+        {
+            Console.WriteLine("Error: Division by zero.");
+            return double.NaN; // повернути значення, яке вказує на помилку
+        }
+        double result = num1 % num2;
+        return result;
+    }
+
+    public static void showCalculation(string operation, double num1, double num2)
+    {
+        double result = 0;
+        switch (operation)
+        {
+            case "add":
+                result = Add(num1, num2);
+                Console.WriteLine($"{num1} + {num2} = {result}");
+                break;
+            case "subtract":
+                result = Subtraction(num1, num2);
+                Console.WriteLine($"{num1} - {num2} = {result}");
+                break;
+            case "multiply":
+                result = Multiplication(num1, num2);
+                Console.WriteLine($"{num1} * {num2} = {result}");
+                break;
+            case "divide":
+                result = Division(num1, num2);
+                Console.WriteLine($"{num1} / {num2} = {result}");
+                break;
+            case "reminder":
+                result = ReminderOfTheDivision(num1, num2);
+                Console.WriteLine($"{num1} % {num2} = {result}");
+                break;
+            default:
+                Console.WriteLine("Invalid operation.");
+                break;
+        }
+    }
 }
